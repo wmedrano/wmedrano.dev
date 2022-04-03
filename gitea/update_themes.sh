d591d3c54e12bee86c320df4f54f726d1e33c5b2
Will Medrano <will.s.medrano@gmail.com>
2022-04-03 20:00:34 +0000

    Add script to update themes.

---
+++ gitea/update_themes.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+run () {
+    GITEA_BASE=$(dirname -- "${BASH_SOURCE[0]}")
+    # If the volume is owned by root, take ownership with:
+    # sudo chown $USER ${GITEA_BASE}/data
+    THEMES_DIR=${GITEA_BASE}/data/gitea/public/css
+    curl https://raw.githubusercontent.com/acoolstraw/earl-grey/master/theme-earl-grey.css >${THEMES_DIR}/theme-earl-grey.css
+    curl https://codeberg.org/Freeplay/Gitea-Modern/src/branch/main/Gitea/theme-gitea-modern.css >${THEMES_DIR}/theme-gitea-modern.css
+    sed -i 's/THEMES.*=.*$/THEMES=gitea,gitea-modern,arc-green,earl-grey/' ${GITEA_BASE}/data/gitea/conf/app.ini | cat
+}
+
+run
