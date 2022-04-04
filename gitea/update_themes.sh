072a39237153c21b88b17cfc447e56f830c3ec04
Will Medrano <will.s.medrano@gmail.com>
2022-04-04 01:03:34 +0000

    Remove unused cat

--- gitea/update_themes.sh
+++ gitea/update_themes.sh
@@ -7,7 +7,7 @@ run () {
     THEMES_DIR=${GITEA_BASE}/data/gitea/public/css
     curl https://raw.githubusercontent.com/acoolstraw/earl-grey/master/theme-earl-grey.css >${THEMES_DIR}/theme-earl-grey.css
     curl https://codeberg.org/Freeplay/Gitea-Modern/src/branch/main/Gitea/theme-gitea-modern.css >${THEMES_DIR}/theme-gitea-modern.css
-    sed -i 's/THEMES.*=.*$/THEMES=gitea,gitea-modern,arc-green,earl-grey/' ${GITEA_BASE}/data/gitea/conf/app.ini | cat
+    sed -i 's/THEMES.*=.*$/THEMES=gitea,gitea-modern,arc-green,earl-grey/' ${GITEA_BASE}/data/gitea/conf/app.ini
 }
 
 run
