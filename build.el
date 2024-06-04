2a6c950c4da1e620fb2d3fb29bdaac4f1606b2c5
wmedrano <will.s.medrano@gmail.com>
2024-06-04 06:55:36 -0700

    Add server setup post.

--- build.el
+++ build.el
@@ -1,7 +1,7 @@
 ;;; package -- Build script for wmedrano.dev
 ;;; Commentary:
 ;;;   Builds wmedrano.dev website by converting .org files into html.
-;;;   emacs -Q --script build.el
+;;;   ~emacs -Q --script build.el~
 ;;; Code:
 (require 'ox-publish)
 
@@ -12,12 +12,12 @@ The static site is output into the site directory."
   (let ((org-html-home/up-format "
 <div id=\"org-div-home-and-up\">
  <!--%s-->
- <a href=\"%s\">HOME</a> | <a href=\"/posts\">POSTS</a> | <a href=\"/about.html\">ABOUT</a>
+ <a href=\"%s\">HOME</a>
 </div>
 ")
         (org-html-link-up "../") ;; Unused. Required for home/up to render.
         (org-publish-project-alist
-         `(("wmedrano-site" :components ("wmedrano-home" "wmedrano-posts" "wmedrano-hosts"))
+         `(("wmedrano-site" :components ("wmedrano-home" "wmedrano-posts"))
            ("wmedrano-home"
             :base-directory "./src"
             :publishing-directory "./src"
@@ -34,24 +34,13 @@ The static site is output into the site directory."
             :publishing-directory "./src/posts"
             :publishing-function org-html-publish-to-html
             :recursive t
-            :auto-sitemap t
             :section-numbers nil
-            :sitemap-title "Posts"
-            :sitemap-filename "index.org"
-            :html-link-home "../"
-            )
-           ("wmedrano-hosts"
-            :base-directory "./src/hosts"
-            :publishing-function org-html-publish-to-html
-            :publishing-directory "./src/hosts"
-            :recursive nil
-            :auto-sitemap t
-            :section-numbers t
-            :sitemap-title "Hosts"
-            :sitemap-filename "index.org"
-            :html-link-home "../")))
+            :html-link-home "../.."
+            )))
         (org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"/style.css\"/>")
-        (org-html-validation-link nil))
+        (org-html-validation-link nil)
+        (org-export-with-author nil)
+        (org-export-with-date t))
     (org-publish-project "wmedrano-site" t)))
 
 (build-wmedrano-dev-site)
